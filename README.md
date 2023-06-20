2431b7e1b394782d1497a285d7a3f8bc6f65bc89
aws69 <awsalomari11469@gmail.com>
2023-06-20 15:23:11 +0300

    lab 11

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# Movies-Library
+# Project Name - Movies Library
+
+**Author Name**: Aws Alomari
+
+## WRRC
+![WWRC](C:\Users\awsal\Videos\Captures\Untitled "wwrc")
+
+## Overview
+An wepsite that shows a list of movies and give the ability to make a favorite list of them .
+
+## Getting Started
+first should be import the express frameWork & cors , then you should store the all method then create a constructor function to ensure your data follow the same format, after that you need to give a PORT to localhost and define the home route, then if you have a lot of pages you need to define them also .
+## Project Features
+-show the list of movies .
+-you can see what's your favorite Movies.
---
+++ server.js
@@ -0,0 +1,55 @@
+"use strict"; 
+
+let express = require("express");
+let cors = require("cors");
+let data = require("./Movie data/data.json");
+let app = express();
+
+
+app.use(cors());
+app.use(express.json());
+
+
+
+app.get('/', handleHome);
+app.get('/favorite', handleFav);
+
+// handle 404 errors
+function handleNotFoud(req, res) {
+    res.send({
+      code: 404,
+      message: "Not Found",
+      extra: "you can visit only home and favorites routes ",
+    });
+  }
+
+// handle 500 errors
+function handleNotFoud(req, res) {
+    res.send({
+      code: 500,
+      message: "Internal server error!"
+    });
+  }
+  
+
+function handleHome(req, res) {
+    res.send({
+        title: data.title,
+        poster_path: data.poster_path,
+        overview: data.overview
+    });
+}
+
+
+
+function handleFav(req, res) {
+    console.log('testing the favorite url');
+    res.send('welcome to favorites');
+}
+
+
+app.listen(3000, startingLog);
+
+function startingLog(req, res) {
+  console.log("Running at 3000");
+}

4c4c92dffa62f09dfd346b188e9c0feab1abb50e
aws69 <awsalomari11469@gmail.com>
2023-06-22 19:57:02 +0300

    lab 12

--- README.md
+++ README.md
@@ -4,12 +4,18 @@
 
 ## WRRC
 ![WWRC](C:\Users\awsal\Videos\Captures\Untitled "wwrc")
+![WWRC]("C:\Users\awsal\Videos\Captures\Untitled "wwrc")
 
 ## Overview
 An wepsite that shows a list of movies and give the ability to make a favorite list of them .
+in our new update you can searsh for movies by there name and it shoes you the trinding list for evry week .
 
 ## Getting Started
 first should be import the express frameWork & cors , then you should store the all method then create a constructor function to ensure your data follow the same format, after that you need to give a PORT to localhost and define the home route, then if you have a lot of pages you need to define them also .
+in the new update you will use 3party API system to connect to other servers and have acces to thir data .
 ## Project Features
 -show the list of movies .
 -you can see what's your favorite Movies.
+-you can see trinding movies for evry week.
+- you can search for movies by there name.
+- you can see movies by there gennras
--- server.js
+++ server.js
@@ -4,7 +4,8 @@ let express = require("express");
 let cors = require("cors");
 let data = require("./Movie data/data.json");
 let app = express();
-
+const axios = require("axios");
+require('dotenv').config();
 
 app.use(cors());
 app.use(express.json());
@@ -13,33 +14,33 @@ app.use(express.json());
 
 app.get('/', handleHome);
 app.get('/favorite', handleFav);
-app.get('/*', handleNotFoud);
 
 // handle 404 errors
-function handleNotFoud(req, res) {
-    res.send({
-      code: 404,
-      message: "Not Found",
-      extra: "you can visit only home and favorites routes ",
-    });
-  }
+app.use((req, res, next) => {
+  res.status(404).send({
+    code: 404,
+    message: "Not Found",
+    extra: "you can visit only home",
+  });
+ 
+});
 
 // handle 500 errors
 app.use((err, req, res, next) => {
   res.status(500).send({
     code: 500,
     message: "Server Error",
-    ahamd: err,
   });
 });
   
 
 function handleHome(req, res) {
-    res.send({
-        title: data.title,
-        poster_path: data.poster_path,
-        overview: data.overview
-    });
+    let movie = new HomeMovie(
+      movieData.title,
+      movieData.poster_path,
+      movieData.overview
+  );
+  res.json(movie);
 }
 
 
@@ -48,6 +49,48 @@ function handleFav(req, res) {
     console.log('testing the favorite url');
     res.send('welcome to favorites');
 }
+
+app.get("/trending",async (req,res)=>{
+  
+  let axiosTrendRespons= await axios.get(`https://api.themoviedb.org/3/trending/all/week?api_key=${process.env.SECRET_API}&language=en-US`);
+  let dataTrindingArr=axiosTrendRespons.data['results'];
+  let trindingArr=[];
+  for (let i =0;i<dataTrindingArr.length; i++){
+    trindingArr.push(dataTrindingArr[i].id,
+      dataTrindingArr[i].title,
+      dataTrindingArr[i].release_date,
+      dataTrindingArr[i].poster_path,
+      dataTrindingArr[i].overview);   
+  }
+  res.send(trindingArr);
+});
+
+app.get("/search", async(req,res)=>{
+  
+  let userData = req.query.name;
+  let axiosSearchRespons = await axios.get(`https://api.themoviedb.org/3/search/movie?api_key=${process.env.SECRET_API}&language=en-US&query=${userData}&page=2`);
+  let dataSearchArr = axiosSearchRespons.data['results'];
+  let searchArr = [];
+  for(let i = 0; i<dataSearchArr.length;i++){
+    searchArr.push(dataSearchArr[i].id,
+      dataSearchArr[i].title,
+      dataSearchArr[i].release_date,
+      dataSearchArr[i],poster_path,
+      dataSearchArr[i].overview);
+  }
+  res.send(searchArr);
+});
+
+app.get("/list",async (req,res)=>{
+  let axiosGenreList=await axios.get(`https://api.themoviedb.org/3/genre/movie/listapi_key=${process.env.SECRET_API}`);
+  let dataListArr = axiosGenreList.data['results'];
+  let listArr=[];
+  for (let i=0; i<dataListArr; i++){
+    listArr.push(dataListArr[i].genre_ids);
+  }
+  res.send();
+});
+
 
 
 app.listen(3000, startingLog);
